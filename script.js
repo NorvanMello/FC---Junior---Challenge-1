59b32529e12536ad545e822f2b4a918ce2a2a40a
NorvanMello <110420594+NorvanMello@users.noreply.github.com>
2026-04-08 15:39:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//hello
+const body = document.querySelector("body");
+const headerBtn = document.querySelector(".header-btn");
+const primaryTextColor = document.querySelectorAll("#text-area-style, .option-sec, .letter-title, .letter-label, .percentage, h1")
+const checkBox = document.querySelectorAll('.option-sec p input[type="checkbox"]');
+
+// const styles = window.getComputedStyle(body);
+
+
+
+headerBtn.addEventListener("click", () => {
+    body.classList.toggle("bg-light")
+
+    primaryTextColor.forEach(el => {
+        el.classList.toggle("text-color")
+    });
+
+    checkBox.forEach(el => {
+        el.classList.toggle("checkBox-light")
+    })
+    
+})
