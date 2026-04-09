2b60caafb1c647011027f64089c1d9fae065364e
NorvanMello <110420594+NorvanMello@users.noreply.github.com>
2026-04-09 16:48:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const body = document.querySelector("body");
 const headerBtn = document.querySelector(".header-btn");
 const headerBtnImg = document.querySelector(".btn-img")
 const logo = document.querySelector(".logo-img");
-const primaryTextColor = document.querySelectorAll("#text-area-style, .option-sec, .letter-title, .letter-label, .percentage, h1");
+
+const textArea = document.querySelector("#text-area-element")
+
 const checkBox = document.querySelectorAll('.option-sec p input[type="checkbox"]');
 
+const letterDensity = document.querySelector(".letter-density");
+const extraContent = document.querySelector(".extra-content");
+
+const toggleBtn = document.querySelector(".toggle-btn")
+
 headerBtn.addEventListener("click", () => {
+    /* Selecting inside the event because before clicking, there is no element created - letter-label for instance */
+    const primaryTextColor = document.querySelectorAll("#text-area-style, .option-sec, .letter-title, .letter-label, .percentage, .toggle-btn, h1");
+
     body.classList.toggle("bg-light")
     headerBtn.classList.toggle("header-btn-light")
 
@@ -23,10 +33,85 @@ headerBtn.addEventListener("click", () => {
             el.classList.toggle("bg-text-area-color")
         }
         el.classList.toggle("text-color")
-        console.dir(el)
     });
 
     checkBox.forEach(el => {
         el.classList.toggle("checkBox-light")
     })
 })
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+const isExpended = true;
+const totalElementsVisible = 5;
+
+let word = [];
+
+textArea.addEventListener("input", (e) => {
+
+    word = e.target.value;
+    
+    console.log(word)
+})
+
+console.log(word)
+
+function renderLetterDensity() {
+    for(let i = 0; i < letters.length; i++) {
+        if(i < 5) {
+            letterDensity.innerHTML += `
+            <ul class="list-container">
+            <li class="list-item">
+                <span class="letter-label">${letters[i].toUpperCase()}</span>
+
+                <div class="progress-bar">
+                  <div class="track-bar">
+                    <div class="fill-bar"></div>
+                  </div>
+                </div>
+
+                <span class="percentage"></span>
+            </li>
+          </ul>
+        `
+        } else {
+            extraContent.innerHTML += `
+                <ul class="list-container">
+                <li class="list-item">
+                    <span class="letter-label">${letters[i].toUpperCase()}</span>
+
+                    <div class="progress-bar">
+                    <div class="track-bar">
+                        <div class="fill-bar"></div>
+                    </div>
+                    </div>
+
+                    <span class="percentage"></span>
+                </li>
+                </ul>
+            `
+
+            toggleBtn.innerHTML = `
+            See more
+            <span class="arrow"></span>`
+        }
+        
+    }
+}
+
+toggleBtn.addEventListener('click', (e) => {
+    extraContent.classList.toggle("block")
+    
+    if(extraContent.classList.contains("block")) {
+        toggleBtn.innerHTML = `
+            See less
+            <span class="arrow open"></span>`
+    } else {
+        toggleBtn.innerHTML = `
+            See more
+            <span class="arrow"></span>` 
+    }
+
+    
+})
+
+renderLetterDensity();
